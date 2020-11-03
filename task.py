414172fc44bcd23f880c799802c3fe57c42ad83d
igortereshchenko <tereshchenko.igor@gmail.com>
2020-11-03 16:21:40 +0200

    task

---
+++ task.py
@@ -0,0 +1,34 @@
+# TODO
+"""
+1. Connection
+
+2. Table
+load table => pd
+
+
+pd = const!!!!!
+
+
+3.1 Bar  + filter
+    pd => sub df
+    df => Bar
+
+3.2 Pie + filter
+    pd => sub df
+    df => Pie
+
+3.3 Scatter + filter
+    pd => sub df
+    df => Scatter
+
+
+4.
+    Create query for bar
+    Create query for pie
+    Create query for scatter
+
+
+"""
+
+df.groupby(['cust_id']).sum()
+
